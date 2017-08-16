78a52c54b5cfb94bbe90291f1516fcb3ffe6f7cd
wind-stone <wind-stone@qq.com>
2017-08-16 17:50:48 +0800

    构建时压缩代码

--- rollup.config.js
+++ rollup.config.js
@@ -1,5 +1,6 @@
-import resolve from 'rollup-plugin-node-resolve';
-import babel from 'rollup-plugin-babel';
+import resolve from 'rollup-plugin-node-resolve'
+import babel from 'rollup-plugin-babel'
+import uglify from 'rollup-plugin-uglify'
 
 export default {
   entry: 'src/index.js',
@@ -9,7 +10,8 @@ export default {
     resolve(),
     babel({
       exclude: 'node_modules/**' // only transpile our source code
-    })
+    }),
+    uglify()
   ],
-  dest: 'dist/input-formatting.js'
+  dest: 'dist/input-formatting.min.js'
 };
